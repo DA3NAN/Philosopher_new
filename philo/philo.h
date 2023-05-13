f1b703249b9c2fd3c48cee072fcd7991b1e99716
DA3NAN <adnane.aitmalek20@gmail.com>
2023-05-13 12:24:31 +0100

    starting

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adnane <adnane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/13 12:16:53 by adnane            #+#    #+#             */
+/*   Updated: 2023/05/13 12:16:59 by adnane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <limits.h>
+
+#endif
